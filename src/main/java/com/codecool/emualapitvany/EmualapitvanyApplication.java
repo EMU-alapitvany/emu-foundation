f7b2080500dc779081f98958d189072d26305bfd
LibLev <libardi.levente@gmail.com>
2020-07-01 10:40:10 +0200

    first commit

---
+++ src/main/java/com/codecool/emualapitvany/EmualapitvanyApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.emualapitvany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmualapitvanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmualapitvanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/emualapitvany/EmualapitvanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.emualapitvany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmualapitvanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
